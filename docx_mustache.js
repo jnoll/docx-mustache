2facc89f2d39eab4d84f6b2d1ad318692cbdfbd9
John Noll <jhnoll@gmail.com>
2020-08-08 13:02:34 +0100

    Initial working version.

---
+++ docx_mustache.js
@@ -0,0 +1,37 @@
+var docx = require('docx-templates').default;
+var YAML = require('yamljs');
+var yargs = require('yargs');	// command line argument parsing
+var stdin = process.stdin;
+var inputChunks = [];
+var fs = require('fs');
+
+// Read yaml from stdin
+const argv = yargs
+    .option('t', {
+        alias: 'template',
+        description: 'template file',
+        type: 'string',
+    })
+    .option('y', {
+        alias: 'yaml',
+        description: 'data in yaml format',
+        type: 'string',
+    })
+    .option('o', {
+        alias: 'output',
+        description: 'output file',
+        type: 'string',
+    })
+    .argv;
+
+var cont = fs.readFileSync(argv.yaml);
+console.log(cont.toString());
+var inputJSON = YAML.parse(cont.toString());
+
+var template = fs.readFileSync(argv.template);
+console.log(inputJSON);
+
+docx({template
+    , data: inputJSON
+    , cmdDelimiter: ['{{', '}}']
+    }).then(result => fs.writeFileSync(argv.output, result));
